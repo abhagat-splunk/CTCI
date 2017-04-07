11df696eb7f8867a1d2d8540893b224ca38eb754
Ankit <ankitbhagat20101994@gmail.com>
2017-04-06 21:08:18 -0400

    Better way to parenthessis

--- Chapter Eight/Parens.java
+++ Chapter Eight/Parens.java
@@ -24,9 +24,37 @@ public class Parens{
 		String right = str.substring(leftIndex+1,str.length());
 		return left+"()"+right;
 	}
+
+
+	public static void addParen(ArrayList<String> list, int leftRem, int rightRem, char[] str, int index){
+		if(leftRem<0||rightRem<leftRem){
+			return;
+		}
+		if(leftRem==0 && rightRem==0){
+			list.add(String.copyValueOf(str));
+		}else{
+			str[index] = '(';
+			addParen(list,leftRem-1,rightRem,str,index+1);
+			str[index] = ')';
+			addParen(list,leftRem,rightRem-1,str,index+1);	
+		}
+	}
+
+	public static ArrayList<String> generateParen(int count){
+		char[] str = new char[count*2];
+		ArrayList<String> list = new ArrayList<String>();
+		addParen(list,count,count,str,0);
+		return list;
+	}
+
+
 	public static void main(String args[]){
 		Set<String> set = generateParens(3);
+		ArrayList<String> al = generateParen(4);
 		for(String str:set){
+			System.out.println(str);
+		}
+		for(String str:al){
 			System.out.println(str);
 		}
 	}
